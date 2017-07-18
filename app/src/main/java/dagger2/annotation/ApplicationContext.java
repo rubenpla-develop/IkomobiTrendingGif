798a036351609f3156b26b57400b76e1fc6a287d
Ruben Pla <ruben.pla@alten.es>
2017-07-18 21:46:44 +0200

    - Annotations added in order to apply dependency injections

---
+++ app/src/main/java/dagger2/annotation/ApplicationContext.java
@@ -0,0 +1,14 @@
+package dagger2.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by alten on 10/7/17.
+ */
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationContext { }
---
+++ app/src/main/java/dagger2/annotation/PerActivity.java
@@ -0,0 +1,15 @@
+package dagger2.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by alten on 10/7/17.
+ */
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+}
