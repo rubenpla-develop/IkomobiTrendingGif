1b43cce45d1721115fe2faa5e4967914d64becca
Ruben Pla <ruben.pla@alten.es>
2017-07-27 16:46:19 +0200

    - Utils class created

---
+++ app/src/main/java/com/rubenpla/develop/ikomobitrendinggif/util/Utils.java
@@ -0,0 +1,45 @@
+package com.rubenpla.develop.ikomobitrendinggif.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alten on 27/7/17.
+ */
+
+public class Utils {
+
+    public ArrayList<String> getUrlGifList(String jsonData) throws JSONException {
+        JSONObject giphy = new JSONObject(jsonData);
+        JSONArray array  = giphy.getJSONArray("data");
+        ArrayList<String> gifURLs = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject o = (JSONObject) array.get(i);
+            JSONObject imagesList  = (JSONObject) o.get("images");
+            JSONObject previewGif  = (JSONObject) imagesList.get("fixed_width_downsampled");
+
+            String url = previewGif.get("url").toString();
+            gifURLs.add(url);
+        }
+
+        return gifURLs;
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+}
