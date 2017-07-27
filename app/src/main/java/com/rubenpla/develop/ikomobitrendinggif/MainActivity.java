530a5f59d5567708a922dc3634c4cafda05f8d11
Ruben Pla <ruben.pla@alten.es>
2017-07-27 16:50:26 +0200

    - MainActivity refactor to MVP pattern

--- app/src/main/java/com/rubenpla/develop/ikomobitrendinggif/MainActivity.java
+++ app/src/main/java/com/rubenpla/develop/ikomobitrendinggif/MainActivity.java
@@ -1,15 +1,19 @@
 package com.rubenpla.develop.ikomobitrendinggif;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.rubenpla.develop.ikomobitrendinggif.adapter.GifGalleryAdapter;
-import com.rubenpla.develop.ikomobitrendinggif.app.IkoApplication;
-import com.rubenpla.develop.ikomobitrendinggif.callback.OnGifsRetrievedListener;
-import com.rubenpla.develop.ikomobitrendinggif.image.ImageLoader;
 import com.rubenpla.develop.ikomobitrendinggif.model.GiphyModel;
+import com.rubenpla.develop.ikomobitrendinggif.util.Utils;
 
 import java.util.List;
 
@@ -17,18 +21,23 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import mvp.presenter.MainActivityPresenter;
+import mvp.view.MainActivityView;
 import dagger2.component.ActivityComponent;
 import dagger2.component.DaggerActivityComponent;
 import dagger2.module.ActivityModule;
 
-public class MainActivity extends AppCompatActivity implements OnGifsRetrievedListener,
-        SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener,
+        MainActivityView {
 
 
     private ActivityComponent activityComponent;
 
     @Inject ImageLoader imageLoader;
     @Inject RecyclerView.LayoutManager layoutManager;
+
+    private MainActivityPresenter mainPresenter;
+    private GifGalleryAdapter adapter;
 
     @BindView(R.id.rv_images)
     RecyclerView recyclerView;
@@ -55,33 +64,84 @@ public class MainActivity extends AppCompatActivity implements OnGifsRetrievedLi
         getActivityComponent().inject(this);
         ButterKnife.bind(this);
 
+        mainPresenter = new MainActivityPresenter(new GiphyModel(), new Utils()); //TODO DEPENDENCY
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
-        imageLoader.getGiphy();
 
         swipeRefreshLayout.setOnRefreshListener(this);
-    }
-
-    @Override
-    public void setGiphyList(List<String> urlGifList) {
-        if (urlGifList != null && urlGifList.size() > 0) {
-            GifGalleryAdapter adapter = new GifGalleryAdapter(this, urlGifList);
-            recyclerView.setAdapter(adapter);
-        }
-
-        swipeRefreshLayout.setRefreshing(false);
+        mainPresenter.onAttach(this);
+        mainPresenter.getViewList(this);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        mainPresenter.onFinish();
         finish();
     }
 
     @Override
     public void onRefresh() {
-        if (imageLoader != null) {
-            imageLoader.getGiphy();
-        }
+        mainPresenter.getViewList(this);
+    }
+
+    @Override
+    public Context getViewContext() {
+        return this;
+    }
+
+    @Override
+    public void loadGifList(final List<String> list) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (adapter != null) {
+                    adapter.setData(getApplicationContext(), list);
+                    swipeRefreshLayout.setRefreshing(false);
+                } else {
+                    adapter = new GifGalleryAdapter(getApplicationContext(), list);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void updateGifList(final List<String> list) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (adapter != null) {
+                    adapter.setData(getApplicationContext(), list);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void showErrorDialog(@StringRes int titleMessage, @StringRes int bodyMessage) {
+        final Resources resources = this.getResources();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle(resources.getString(titleMessage))
+                .setMessage(resources.getString(bodyMessage))
+                .setIcon(R.mipmap.ic_launcher_round)
+                .setNeutralButton(resources.getString(R.string.dialog_button_accept),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(),
+                                        resources.getString(R.string.img_loader_dialog_warning_toast_message),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setCancelable(false);
+
+        builder.show();
+    }
+
+    @Override
+    public void showToastMessage(@StringRes int message) {
+        Toast.makeText(this, getResources().getString(message), Toast.LENGTH_LONG).show();
     }
 }
